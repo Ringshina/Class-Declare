dd0b93df5d9e24c0c81181f00c098f02be5723ea
USER <USER@DESKTOP-OEBF7M3>
2023-12-01 23:13:30 +0600

    Add project files.

---
+++ tithi33/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tithi33
+{
+    public class snearaself
+    {
+        public string firstName;
+        public string lastName;
+        public void Lover()
+        {
+            Console.WriteLine("Sneara likes " + " " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            snearaself male = new snearaself();
+            male.firstName = "Abul Ala";
+            male.lastName = "Walid";
+            male.Lover();
+        }
+    }
+}
